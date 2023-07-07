6a187c9403cfc4b752d32fe087ef956d75a69498
Deepak Duggirala <ddeepak6992@gmail.com>
2023-07-07 13:53:00 -0400

    expiry time based on client scope

--- signet/oauth2_server.py
+++ signet/oauth2_server.py
@@ -1,6 +1,8 @@
+from authlib.common.security import generate_token
 from authlib.integrations.sqla_oauth2 import create_query_client_func, create_revocation_endpoint, \
     create_query_token_func
 from authlib.oauth2.rfc6749 import grants
+from authlib.oauth2.rfc6750 import BearerTokenGenerator
 
 from signet.models import db, OAuth2Token, OAuth2Client
 from signet.oauth2.authorization_server import AuthorizationServer
@@ -39,9 +41,28 @@ authorization = AuthorizationServer(
 def config_oauth(app):
     authorization.init_app(app)
     authorization.register_grant(grants.ClientCredentialsGrant)
+    authorization.register_token_generator(grants.ClientCredentialsGrant.GRANT_TYPE, create_token_generator())
 
     # support revocation
     revocation_cls = create_revocation_endpoint(db.session, OAuth2Token)
     authorization.register_endpoint(revocation_cls)
 
     # support introspection
+
+
+def create_token_generator():
+    def token_generator(*args, **kwargs):
+        return generate_token(length=42)
+
+    def expires_generator(client, grant_type):
+        client_scopes = client.scope.split(' ')
+        if 'download_file' in client_scopes:
+            return 30
+        else:
+            return 864000
+
+    return BearerTokenGenerator(
+        access_token_generator=token_generator,
+        refresh_token_generator=None,
+        expires_generator=expires_generator
+    )
